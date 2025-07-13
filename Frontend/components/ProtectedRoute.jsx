165a4d6c77ea5b0156472ff6ad86c743cdd9152d
Electron <yogesh.dubey.0804@gmail.com>
2025-07-13 12:37:08 +0530

    Remove unused files and components from the Frontend directory, including App.css, App.jsx, main.jsx, supabase.js, vite.config.js, and SVG assets. This cleanup helps streamline the project by eliminating unnecessary code and resources.

--- Frontend/components/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-// ProtectedRoute.jsx
-import { Navigate } from "react-router-dom";
-
-export default function ProtectedRoute({ children }) {
-  const isAuthenticated = localStorage.getItem("isAuthenticated");
-
-  return isAuthenticated ? children : <Navigate to="/login" />;
-}
--- Frontend/src/supabase.js
+++
@@ -1,7 +0,0 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ynyatndehtvknyaewbif.supabase.co/';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlueWF0bmRlaHR2a255YWV3YmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIzMzA2NzIsImV4cCI6MjA2NzkwNjY3Mn0.Pe9-FzkhuANpUNkQY96RIcCz6dnNzjkzWwQRCkJgrek';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
